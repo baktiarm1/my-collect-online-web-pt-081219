a603ce897866b054e691304036f210b9f9a5b442
Baktiar Miah <baktiar.m@gmail.com>
2019-08-27 20:18:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
-
-def my_collect(arr)
-  counter=0 
-  
-   while counter > arr.length 
-  end 
-
-end 
+def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
+end
